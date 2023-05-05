806f5ee256ec1125d85cf6f9e60e389680950bcc
Vladimir <troyanskii1998@yandex.ru>
2023-05-06 02:58:43 +0300

    добавляет игровые ячейки поля

--- main.js
+++ main.js
@@ -7,24 +7,45 @@ const cell = document.querySelector('.cell');
 
 
 
-const setGameFieldSize = () => {
-    
-}
-
-
-
 window.addEventListener('load', () => {
-    const pageWidth = document.documentElement.scrollWidth;
-    const pageHeight = document.documentElement.scrollHeight;
-    
-    box.style.width = `${0.8 * pageWidth}px`;
-    box.style.height = `${0.8 * pageWidth}px`;    
-})
+  const pageWidth = document.documentElement.scrollWidth;
+  const pageHeight = document.documentElement.scrollHeight;
 
-window.addEventListener('resize', () => {
-    const pageWidth = document.documentElement.scrollWidth;
-    const pageHeight = document.documentElement.scrollHeight;
-    
+  if (pageWidth <= pageHeight) {
     box.style.width = `${0.8 * pageWidth}px`;
     box.style.height = `${0.8 * pageWidth}px`;
+  } else {
+    box.style.width = `${0.8 * pageHeight}px`;
+    box.style.height = `${0.8 * pageHeight}px`;
+  }
+
 })
+
+
+window.addEventListener('resize', () => {
+  const pageWidth = document.documentElement.scrollWidth;
+  const pageHeight = document.documentElement.scrollHeight;
+
+  if (pageWidth <= pageHeight) {
+    box.style.width = `${0.8 * pageWidth}px`;
+    box.style.height = `${0.8 * pageWidth}px`;
+  } else {
+    box.style.width = `${0.8 * pageHeight}px`;
+    box.style.height = `${0.8 * pageHeight}px`;
+  }
+})
+
+
+
+
+
+
+
+const playingFields = box.querySelectorAll('.cell[data-field]');
+
+playingFields.forEach((element) => {
+  element.style.backgroundColor = 'green';
+
+  element.textContent = `${element.getAttribute('data-field')}`;
+})
+
